e7e30b8dbf62fcbd8e740ed5be06a9b1fc5f8fea
retsu <teck.geek1991@gmail.com>
2026-09-15 22:10:00 -0600

    Added final project items

--- CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
+++ CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
@@ -274,7 +274,7 @@ void SceneManager::RenderScene()
 	/*** Drawwing the box on the left, the small one on its side ***/
 	/******************************************************************/
 	// set the XYZ scale for the mesh
-	scaleXYZ = glm::vec3(4.0f, 1.0f, 1.0f);
+	scaleXYZ = glm::vec3(3.5f, 2.0f, 1.0f);
 
 	// set the XYZ rotation for the mesh
 	XrotationDegrees = 0.0f;
