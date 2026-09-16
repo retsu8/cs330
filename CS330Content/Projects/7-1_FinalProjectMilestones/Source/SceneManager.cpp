50bc901fc6c922076ac8578fbc0314fb0ad4684d
retsu <teck.geek1991@gmail.com>
2026-09-16 17:06:59 -0600

    the cylinder is loner and wider now

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -515,7 +515,7 @@ void SceneManager::RenderScene()
 	/*** Creating the handle going down from the cylinder  ***/
 	/******************************************************************/
 	// set the XYZ scale for the mesh
-	scaleXYZ = glm::vec3(0.3f, 1.2f, 0.3f);
+	scaleXYZ = glm::vec3(0.35f, 1.4f, 0.35f);
 
 	// set the XYZ rotation for the mesh
 	XrotationDegrees = 0.0f;
