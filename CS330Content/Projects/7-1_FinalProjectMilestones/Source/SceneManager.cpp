43d9d094eea88d73b17ad00a0c2dee38c9b8542c
retsu <teck.geek1991@gmail.com>
2026-09-16 15:58:57 -0600

    fix the float

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -469,7 +469,7 @@ void SceneManager::RenderScene()
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
-	positionXYZ = glm::vec3(-4.0f, 0.0f, -6f);
+	positionXYZ = glm::vec3(-4.0f, 0.0f, -6.0f);
 
 	// set the transformations into memory to be used on the drawn meshes
 	SetTransformations(
