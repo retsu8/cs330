0ea803567a18190062805c1fe2168efd7f9ab593
retsu <teck.geek1991@gmail.com>
2026-09-16 16:03:33 -0600

    reposition the plane

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -441,7 +441,7 @@ void SceneManager::RenderScene()
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
-	positionXYZ = glm::vec3(0.0f, 0.0f, 0.0f);
+	positionXYZ = glm::vec3(0.0f, 9.0f, -10.0f);
 
 	// set the transformations into memory to be used on the drawn meshes
 	SetTransformations(
