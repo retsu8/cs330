aa29ceac524bf7cf1399e53c9fc9ce7fbfacf44a
retsu <teck.geek1991@gmail.com>
2026-09-15 21:05:54 -0600

    make the box a little smaller

--- CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
+++ CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
@@ -273,7 +273,7 @@ void SceneManager::RenderScene()
 	/*** Drawwing the box in the middle that is square  ***/
 	/******************************************************************/
 	// set the XYZ scale for the mesh
-	scaleXYZ = glm::vec3(8.0f, 8.0f, 8.0f);
+	scaleXYZ = glm::vec3(6.0f, 6.0f, 6.0f);
 
 	// set the XYZ rotation for the mesh
 	XrotationDegrees = 0.0f;
