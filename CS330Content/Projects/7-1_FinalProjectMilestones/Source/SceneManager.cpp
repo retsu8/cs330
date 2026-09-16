3191a7df21d91747ec434c7f9fda9cbda4bb49a0
retsu <teck.geek1991@gmail.com>
2026-09-16 15:51:57 -0600

    rotate the right way

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -436,8 +436,8 @@ void SceneManager::RenderScene()
 	scaleXYZ = glm::vec3(20.0f, 1.0f, 10.0f);
 
 	// set the XYZ rotation for the mesh
-	XrotationDegrees = 0.0f;
-	YrotationDegrees = 90.0f;
+	XrotationDegrees = 90.0f;
+	YrotationDegrees = 0.0f;
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
