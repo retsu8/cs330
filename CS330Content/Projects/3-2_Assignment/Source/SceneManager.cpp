6ae6140e21853802b4dba94a3dc20b796702a4d3
retsu <teck.geek1991@gmail.com>
2026-09-15 21:46:47 -0600

    reposition the sphere and moved the box a back

--- CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
+++ CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
@@ -255,7 +255,7 @@ void SceneManager::RenderScene()
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
-	positionXYZ = glm::vec3(5.5f, -1.0f, 2.5f);
+	positionXYZ = glm::vec3(5.5f, 0.0f, 1.3f);
 
 	// set the transformations into memory to be used on the drawn meshes
 	SetTransformations(
