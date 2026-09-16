b9bbf90e148353f4df8c25ac53794af2ab86881a
retsu <teck.geek1991@gmail.com>
2026-09-15 19:50:50 -0600

    rotate the square a bit more

--- CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
+++ CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
@@ -250,7 +250,7 @@ void SceneManager::RenderScene()
 
 	// set the XYZ rotation for the mesh
 	XrotationDegrees = 0.0f;
-	YrotationDegrees = 95.0f;
+	YrotationDegrees = 97.0f;
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
