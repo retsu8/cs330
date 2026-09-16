e1a387bb7dec542f9b1d57a6ef9ab7ffaa410bc2
retsu <teck.geek1991@gmail.com>
2026-09-16 16:58:33 -0600

    tone down the alpha

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -495,6 +495,8 @@ void SceneManager::RenderScene()
 	YrotationDegrees = 0.0f;
 	ZrotationDegrees = 90.0f;
 
+	GL_SHININESS = 3
+
 	// set the XYZ position for the mesh
 	positionXYZ = glm::vec3(8.0f, 3.8f, 0.0f);
 
