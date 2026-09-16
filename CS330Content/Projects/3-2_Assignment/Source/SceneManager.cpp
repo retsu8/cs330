761bdaccfb17e7ebaa14b3495790881ed4c83c33
retsu <teck.geek1991@gmail.com>
2026-09-15 19:39:07 -0600

    scaling my square

--- CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
+++ CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
@@ -246,7 +246,7 @@ void SceneManager::RenderScene()
 	/*** Drawwing the box on the right, the tall one  ***/
 	/******************************************************************/
 	// set the XYZ scale for the mesh
-	scaleXYZ = glm::vec3(5.0f, 9.0f, 1.3f);
+	scaleXYZ = glm::vec3(8.0f, 1.0f, 12.0f);
 
 	// set the XYZ rotation for the mesh
 	XrotationDegrees = 0.0f;
