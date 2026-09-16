fea7eabc6fb5c903d9b659755821443587c57ee0
retsu <teck.geek1991@gmail.com>
2026-09-16 15:55:16 -0600

    Move cylender and resize

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -461,7 +461,7 @@ void SceneManager::RenderScene()
 	/*** Creating the base for the coffee maker out of a cylinder   ***/
 	/******************************************************************/
 	// set the XYZ scale for the mesh
-	scaleXYZ = glm::vec3(3.2f, 6.0f, 3.2f);
+	scaleXYZ = glm::vec3(0.9f, 2.8f, 0.9f);
 
 	// set the XYZ rotation for the mesh
 	XrotationDegrees = 0.0f;
