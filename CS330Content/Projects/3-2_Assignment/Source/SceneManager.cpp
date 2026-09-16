65d60b594f5a13cda7958d4d4af01316251587c7
retsu <teck.geek1991@gmail.com>
2026-09-15 19:31:59 -0600

    moving a box -5f on y

--- CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
+++ CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
@@ -254,7 +254,7 @@ void SceneManager::RenderScene()
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
-	positionXYZ = glm::vec3(0.2f, 0.0f, 2.0f);
+	positionXYZ = glm::vec3(5.0f, 1.0f, 2.0f);
 
 	// set the transformations into memory to be used on the drawn meshes
 	SetTransformations(
