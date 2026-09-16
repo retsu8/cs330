03f52624e749155980751953aa98e8b1b62a0098
retsu <teck.geek1991@gmail.com>
2026-09-16 16:39:31 -0600

    move up by 3

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -496,7 +496,7 @@ void SceneManager::RenderScene()
 	ZrotationDegrees = 90.0f;
 
 	// set the XYZ position for the mesh
-	positionXYZ = glm::vec3(0.0f, 3.0f, 0.0f);
+	positionXYZ = glm::vec3(4.0f, 4.2f, 0.0f);
 
 	// set the transformations into memory to be used on the drawn meshes
 	SetTransformations(
