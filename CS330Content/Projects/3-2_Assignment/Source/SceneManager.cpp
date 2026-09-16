301dfb57e823b5a0cd49ae530a48d6a6d06adc40
retsu <teck.geek1991@gmail.com>
2026-09-15 21:51:08 -0600

    fix the gloat

--- CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
+++ CS330Content/Projects/3-2_Assignment/Source/SceneManager.cpp
@@ -247,7 +247,7 @@ void SceneManager::RenderScene()
 	/*** Drawwing the box on the right, the tall one the cereal box ***/
 	/******************************************************************/
 	// set the XYZ scale for the mesh
-	scaleXYZ = glm::vec3(2.0f, 12.0f, 5.0f);
+	scaleXYZ = glm::vec3(2.0f, 14.0f, 5.0f);
 
 	// set the XYZ rotation for the mesh
 	XrotationDegrees = 0.0f;
@@ -308,7 +308,7 @@ void SceneManager::RenderScene()
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
-	positionXYZ = glm::vec3(1.7f, 0.0f, 5.2f);
+	positionXYZ = glm::vec3(1.7f, 1.0f, 4.5f);
 
 	// set the transformations into memory to be used on the drawn meshes
 	SetTransformations(
