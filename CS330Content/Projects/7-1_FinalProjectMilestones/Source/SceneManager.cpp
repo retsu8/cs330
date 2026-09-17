97d5855599a53c07fde30da467c784f21f8c1fa9
retsu <teck.geek1991@gmail.com>
2026-09-16 18:11:19 -0600

    add a cylinder to be skinny

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -606,7 +606,7 @@ void SceneManager::RenderScene()
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
-	positionXYZ = glm::vec3(9.2f, 4.7f, 0.0f);
+	positionXYZ = glm::vec3(9.0f, 4.9f, 0.0f);
 
 	// set the transformations into memory to be used on the drawn meshes
 	SetTransformations(
