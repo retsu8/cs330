774f015527b05930cd26b0b21b2565886032baaa
retsu <teck.geek1991@gmail.com>
2026-09-18 20:01:11 -0600

    cut it in half turn it and make the tapered cylender sideways

--- CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
+++ CS330Content/Projects/7-1_FinalProjectMilestones/Source/SceneManager.cpp
@@ -653,15 +653,15 @@ void SceneManager::RenderScene()
 	/*** Adding the spout ***/
 	/******************************************************************/
 	// set the XYZ scale for the mesh
-	scaleXYZ = glm::vec3(1.0f, 1.0f, 1.0f);
+	scaleXYZ = glm::vec3(1.0f, 0.5f, 1.0f);
 
 	// set the XYZ rotation for the mesh
-	XrotationDegrees = 0.0f;
+	XrotationDegrees = 90.0f;
 	YrotationDegrees = 0.0f;
 	ZrotationDegrees = 0.0f;
 
 	// set the XYZ position for the mesh
-	positionXYZ = glm::vec3(0.0f, 0.0f, 0.0f);
+	positionXYZ = glm::vec3(0.0f, 5.0f, 0.0f);
 
 	// set the transformations into memory to be used on the drawn meshes
 	SetTransformations(
